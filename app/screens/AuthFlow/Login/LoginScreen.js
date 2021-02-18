811a2ae636200a886b961c28774c266400eed401
Mehdi <mehdiparsaeigm@gmail.com>
2021-02-19 00:34:00 +0330

    Splash Done!

--- app/screens/AuthFlow/Login/LoginScreen.js
+++ app/screens/AuthFlow/Login/LoginScreen.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import { Text, View } from 'react-native';
-import i18n from '../../../hooks/useApi';
 import styles from './styles';
 
 function LoginScreen({ }) {
     return (
         <View style={styles.container}>
             <Text>Login Screen</Text>
-            <Text>{i18n.t('login')}</Text>
-
         </View>
     );
 }
